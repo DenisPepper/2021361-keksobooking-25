ff0ad73cd4e8bb4a6d3c50a6e158cb0aa519c789
Maxim Volkov <mx_ruz_net@hotmail.com>
2022-02-25 11:18:26 +0300

    step 2

--- js/main.js
+++ js/main.js
@@ -10,6 +10,9 @@ function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   if (min < 0 || max < 0 || min >= max) {
+    // Это поведение принято в Си, там плохо с исключениями (раньше было)
+    // в последнее время в этом месте сообщество склоняется к мысли
+    // что надо throw new Error('неверный аргумент')
     return -1;
   }
   return Math.floor(Math.random() * (max - min + 1)) + min;
